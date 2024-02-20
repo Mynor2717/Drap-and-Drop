747dd7d7831f4494edf4de4138ed89dfbe5576d4
node <ratafak15@gmail.com>
2024-02-19 20:24:16 -0500

    Funcion lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { DragDropContext } from "@hello-pangea/dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 
 const inicialTodos = [
@@ -12,19 +12,47 @@ const inicialTodos = [
 const App = () => {
 
   const [todos, setTodos] = useState(inicialTodos)
-  return (
-    <DragDropContext>
-      <h1>Todo</h1>
 
-      <ul>
+  const habldeDragEnd = (result) => {
+    if (!result.destination) return;
+    const startIndex = result.source.index;
+    const endIndex = result.destination.index;
 
-        {todos.map((todo) => (
-            <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
+  };
 
-    </DragDropContext>
-  )
+
+return (
+  <DragDropContext onDragEnd={habldeDragEnd}>
+    <h1>Todo</h1>
+    <Droppable droppableId="todos">
+      {
+        (droppableProvider) => (
+          <ul ref={droppableProvider.innerRef}
+            {...droppableProvider.droppableProps}>
+
+            {todos.map((todo, index) => (
+              <Draggable index={index} key={todo.id} draggableId={`${todo.id}`}>
+                {
+                  (draggableProvider) => (
+                    <li
+                      ref={draggableProvider.innerRef}
+                      {...draggableProvider.dragHandleProps}
+                      {...draggableProvider.draggableProps}
+                    >{todo.text}</li>
+                  )
+
+                }
+
+              </Draggable>
+              //Siempre que se envuelva un elemento hay que mover la Key al elemento principal
+            ))}
+            {droppableProvider.placeholder}
+          </ul>
+        )
+      }
+    </Droppable>
+  </DragDropContext>
+)
 
 }
 
